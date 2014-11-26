46cd455e80a2fe8e52b700c14b71223bcfe3b0de
lazio14 <laziofly@yeah.net>
2014-11-26 23:46:21 +0800

    学习使用open create handle系列函数。

--- WindowsViaCppPratise/singleton/singleton.cpp
+++ WindowsViaCppPratise/singleton/singleton.cpp
@@ -17,6 +17,30 @@ int APIENTRY WinMain(HINSTANCE hInstance,
 		return 0;
 	}
 
+	HANDLE hNotExistHandle = OpenMutex(NULL, FALSE, TEXT("{this is not exist handle}"));
+	if (hNotExistHandle == NULL)
+	{
+		if (GetLastError() == ERROR_FILE_NOT_FOUND)
+		{
+			MessageBox(NULL, TEXT("the handle not exist"), TEXT("WARNING"), MB_OK);
+		}
+	}
+	else
+	{
+		CloseHandle(hNotExistHandle);
+	}
+
+	HANDLE hOtherTypeHandle = CreateFileMapping((HANDLE)0xFFFFFFFF,NULL, PAGE_READWRITE,0,1024,TEXT("{THIS IS A UNIQE STRING}"));
+
+	if (hOtherTypeHandle == NULL)
+	{
+		if (GetLastError() == ERROR_INVALID_HANDLE)
+		{
+			MessageBox(NULL, TEXT("handle type not match"), TEXT("WARNING"), MB_OK);
+		}
+	}
+
+	
 	Sleep(1000000);
 	CloseHandle(h);
 	return 0;
