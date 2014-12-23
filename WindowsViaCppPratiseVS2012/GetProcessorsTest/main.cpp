21ac61e67f54f7e862f4c0f648114fbbded3486c
lazio14 <laziofly@yeah.net>
2014-12-23 23:14:37 +0800

    获取处理器core的数量。

---
+++ WindowsViaCppPratiseVS2012/GetProcessorsTest/main.cpp
@@ -0,0 +1,50 @@
+#include <windows.h>
+#include <iostream>
+using namespace std;
+
+void ShowProcessors()
+{
+	PSYSTEM_LOGICAL_PROCESSOR_INFORMATION pBuffer = NULL;
+	DWORD dwSize = 0;
+	DWORD procCoreCount = 0;
+
+	BOOL bResult = GetLogicalProcessorInformation(pBuffer, &dwSize);
+	if (GetLastError() != ERROR_INSUFFICIENT_BUFFER)
+	{
+		cout << "impossible get processors info" << endl;
+	}
+	pBuffer = (PSYSTEM_LOGICAL_PROCESSOR_INFORMATION)malloc(dwSize);
+	bResult = GetLogicalProcessorInformation(pBuffer, &dwSize);
+	if (!bResult)
+	{
+		free(pBuffer);
+		return;
+	}
+	DWORD lpiCount = dwSize / sizeof(SYSTEM_LOGICAL_PROCESSOR_INFORMATION);
+	for (DWORD current = 0;current < lpiCount;current++)
+	{
+		if (pBuffer[current].Relationship == RelationProcessorCore)
+		{
+			if (pBuffer[current].ProcessorCore.Flags == 1)
+			{
+				cout << "one cpu core" << endl;
+			}
+			else
+			{
+				cout << "one cpu socket" << endl;
+			}
+			procCoreCount++;
+		}
+	}
+
+	cout << "core sum:" << procCoreCount << endl;
+	free(pBuffer);
+}
+
+void main()
+{
+	ShowProcessors();
+	system("pause");
+	BOOL bIsWow = FALSE;
+	IsWow64Process(GetCurrentProcess(), &bIsWow);
+}
