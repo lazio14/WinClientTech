c1714b669ffaeb7d8e1c7f853a419b372e9be079
lazio14 <laziofly@yeah.net>
2015-01-08 22:32:40 +0800

    add

--- WindowsViaCppPratiseVS2012/Funcenstein/main.cpp
+++ WindowsViaCppPratiseVS2012/Funcenstein/main.cpp
@@ -1,24 +1,40 @@
+#include <windows.h>
 #include <iostream>
 using namespace std;
 
+HANDLE g_sem = NULL;
+
+DWORD WINAPI ChildThread(LPVOID pvParam)
+{
+	WaitForSingleObject(g_sem, INFINITE);
+	cout << "child thread" << endl;
+	return 0;
+}
+
 int funcenstein()
 {
+	ReleaseSemaphore(g_sem, 1, NULL);
 	int retVal = 5;
 	__try
 	{
-		goto ReturnVal;
+		WaitForSingleObject(g_sem, INFINITE);
+		PVOID p = NULL;
+		int a = *(int *)p;
+		cout << "ok" << endl;
 	}
 	__finally
 	{
-		retVal = 10;
+		ReleaseSemaphore(g_sem, 1, NULL);
 	}
 
-ReturnVal:
 	return retVal;
 }
 
 void main()
 {
+	g_sem = CreateSemaphore(NULL, 0, 1, NULL);
 	cout << funcenstein() << endl;
+	CreateThread(NULL, 0, ChildThread, NULL, 0, NULL);
 	system("pause");
+	
 }
